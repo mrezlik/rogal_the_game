438d7d3f37e703eafa96c2984dcfec6623bacd02
mrezlik <mrezlik@gmail.com>
2017-08-18 10:27:11 +0200

    Add short story at the beginning

--- game.py
+++ game.py
@@ -1,12 +1,14 @@
 import os
 from controls import getch
 from map import create_board, print_board
-from introduction import print_logo
+from introduction import print_logo, story
 import time
 
 def main():
 	print_logo()
 	time.sleep(3)
+	story()
+	time.sleep(5)
 	map_one = create_board(80, 20)
 	x = None
 	while not(x == 'q'):
--- introduction.py
+++ introduction.py
@@ -8,3 +8,13 @@ def print_logo():
     ╚══════╝   ╚═╝   ╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝  ╚═╝╚══════╝
     """]
     print(*logo)
+
+
+def story():
+    print("""It is the year 2100. Poland has become a world power.
+    Out of distant planets there are strange messages about the
+    presence of non-human civilization. We, as the best Polish citizen, are setting out on a
+    "peaceful" mission to solve these problems. With the development of the situation,
+    you notice that something is wrong. The alien are fighting very fiercely and
+    apparently guarding something ... Is this the end of the present world coming?
+    You need to find out what's going on and save the earth at all costs.""")
