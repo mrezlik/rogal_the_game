51ac05c8e04374418c8424539def598e3648f514
mrezlik <mrezlik@gmail.com>
2017-08-18 10:20:30 +0200

    Add 'logo' at the beginning

--- game.py
+++ game.py
@@ -1,8 +1,12 @@
 import os
 from controls import getch
 from map import create_board, print_board
+from introduction import print_logo
+import time
 
 def main():
+	print_logo()
+	time.sleep(3)
 	map_one = create_board(80, 20)
 	x = None
 	while not(x == 'q'):
