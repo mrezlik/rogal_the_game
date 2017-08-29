d59d4e177087a2e1945227b0f0dc8813eef76529
Vernitrax <ty21@op.pl>
2017-08-29 15:31:30 +0200

    upgraded some solutions in fight

--- battle.py
+++ battle.py
@@ -16,10 +16,10 @@ def fight(player, monster):
 			player.exp += monster.exp
 			
 			if '|' not in player.strongest_monster_killed:
-				player.strongest_monster_killed = monster.name + '|' + str(monster.exp)
+				player.strongest_monster_killed = monster.name + '|' + ' ' * (5 - len(str(monster.exp))) + str(monster.exp)
 			else:
-				if monster.exp > int(player.strongest_monster_killed[-3:]):
-					player.strongest_monster_killed = monster.name + '|' + str(monster.exp)
+				if monster.exp > int(player.strongest_monster_killed[-5:]):
+					player.strongest_monster_killed = monster.name + '|' + ' ' * (5 - len(str(monster.exp))) + str(monster.exp)
 			
 			return player
 			
