ca4294280e633f434ec3cec32b3081a4b0bd3f29
Vernitrax <ty21@op.pl>
2017-08-30 09:32:25 +0200

    added leveling system to Player class

--- character.py
+++ character.py
@@ -12,11 +12,24 @@ class Player(Character):
 	
 	exp = 0
 	strongest_monster_killed = 'You did not killed anything!'
+	level = 0
+	next_level = 100
 	
 	def __init__(self, name, hp, damage):
 		self.name = name
 		self.hp = hp
 		self.damage = damage
+	
+	def check_level(self):
+		if self.exp >= self.next_level:
+			self.levelup()
+			self.check_level()
+			
+	def levelup(self):
+		self.level += 1
+		self.next_level += (self.level + 1) * 100
+		self.hp += 25
+		self.damage += 5
 
 
 class Enemy(Character):
@@ -29,10 +42,15 @@ class Enemy(Character):
 
 
 def main():
-	player = Character('Player', 100, 15)
-	print(player.name, player.hp, player.damage, player.alive)
-	alien = Enemy('Alien', 50, 15, 100)
-	print(alien.name, alien.hp, alien.damage, alien.alive, alien.exp)
+	player = Player('Zdzisiek', 100, 15)
+	print(player.name, player.hp, player.damage, player.alive, player.exp, player.level, player.next_level)
+	player.exp += 800
+	player.check_level()
+	print(player.name, player.hp, player.damage, player.alive, player.exp, player.level, player.next_level)
+	player.exp += 200
+	player.check_level()
+	print(player.name, player.hp, player.damage, player.alive, player.exp, player.level, player.next_level)
+
 	
 if __name__ == '__main__':
 	main()
